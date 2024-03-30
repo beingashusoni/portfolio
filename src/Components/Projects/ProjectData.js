b15a550667fc06ce519f403785323e23ade3e2d1
Ashutosh Soni <84983161+beingashusoni@users.noreply.github.com>
2024-03-31 00:42:03 +0530

    Add files via upload

---
+++ src/Components/Projects/ProjectData.js
@@ -0,0 +1,53 @@
+import ZuppImg from "./ProjectImg/project1.jpg"; 
+import PortfolioImg from "./ProjectImg/Project2.jpg";
+import DribbleImg from "./ProjectImg/project4.jpg"; 
+import WeatherAppImg from "./ProjectImg/project5.jpg";
+import OesImg from "./ProjectImg/project3.jpg";
+import MrsImg from "./ProjectImg/project6.jpg";
+
+
+export const MernProjectData = [
+  {
+    img: ZuppImg,
+    title: "Zuppp! - Upload, Share & Download File",
+    techstack: "MongoDB, Express.js, React.js, Node.js, Tailwind CSS",
+    // live: "https://density-exchange-task-six.vercel.app/",
+    github: "https://github.com/beingashusoni/Zuppp_FileSharing",
+  },
+  {
+    img: PortfolioImg,
+    title: "Personal Portfolio",
+    techstack: "React.js, Node.js, Tailwind CSS, Vite.js, Emailjs",
+    // live: "https://density-exchange-task-six.vercel.app/",
+    github: "https://github.com/beingashusoni/portfolio/",
+  },
+  {
+    img: OesImg,
+    title: "Online Examination System",
+    techstack: " HTML, CSS, JavaScript, Bootstrap, MySQL, PHP",
+    // live: "https://summarizearticle.netlify.app/",
+    github: "https://github.com/beingashusoni/OnlineExaminationSystem",
+  },
+  {
+    img: DribbleImg,
+    title: "Dribbble Template Design",
+    techstack: "HTML, CSS, JavaScript, React, Tailwind CSS",
+    live: "https://density-exchange-task-six.vercel.app/",
+    github: "https://github.com/beingashusoni/DensityExchangeTask/tree/main",
+  },
+  {
+    img: WeatherAppImg,
+    title: "Weather App using API",
+    techstack: "HTML, CSS, JavaScript, API",
+    live: "https://6559e7c134e0761d6e2c6a92--ephemeral-choux-f4f8ed.netlify.app/",
+    github: "https://github.com/beingashusoni/WeatherApp",
+  },
+  
+  {
+    img: MrsImg,
+    title: "Movie Recommendation System",
+    techstack: " Python, Python-Libraries, Dataset of movies",
+    // live: "https://pankajktechblog.vercel.app/",
+    github: "https://github.com/beingashusoni/MRS",
+  },
+];
