c28e42eba0175a58652bf66629d02bc39ff3ecab
Ashutosh Soni <84983161+beingashusoni@users.noreply.github.com>
2024-08-30 13:04:08 +0530

    Update WorkExp.jsx

--- src/Components/Experience/WorkExp.jsx
+++ src/Components/Experience/WorkExp.jsx
@@ -42,14 +42,16 @@ const WorkExp = () => {
                 BrainOwls Technology - Bhopal
               </Typography>
               <Typography className=" my-2" color={darkMode ? 'white' : 'gray'}>
-                <span style={{ fontWeight: 'bold' }}>Software Developer Intern</span> <br /> Jan'2024 - Present
+                <span style={{ fontWeight: 'bold' }}>Software Developer Intern (Frontend)</span> <br /> Jan'2024 - Jul'2024
               </Typography>
             </div>
           </TimelineHeader>
           <TimelineBody className="pb-8">
             <ul className={`list-disc font-sans ml-5 ${darkMode ? 'text-white' : 'text-gray-800'}`}>
-              <li>Responsible for developing front-end for numerous websites, optimizing user experience and functionality.</li>
-              <li>Managed UI/UX designing, achieving a 20% increase in user satisfaction.</li>
+              <li>•Designed 50+ UI components in Figma for the Society Management System, enhancing user satisfaction by 30%.</li>
+              <li>Developed 20+ responsive web pages using JavaScript, Tailwind CSS, React & Redux.</li>
+              <li>Collaborated with a team of 4, ensuring a 90% on-time delivery, through effective communication and coordination.</li>
+              <li>•Utilized Jira and Trello for task management, achieving a 92% completion rate per sprint and maintaining comprehensive documentation.</li>
             </ul>
           </TimelineBody>
         </TimelineItem>
@@ -71,15 +73,15 @@ const WorkExp = () => {
                 Swaayam - Bengaluru
               </Typography>
               <Typography className=" my-2" color={darkMode ? 'white' : 'gray'}>
-                <span style={{ fontWeight: 'bold' }}>Intern</span> <br /> Mar'2023 - Aug'2023 (6 Months)
+                <span style={{ fontWeight: 'bold' }}>Technical Intern (Part-Time)</span> <br /> Nov'2021 - Aug'2024
               </Typography>
             </div>
           </TimelineHeader>
           <TimelineBody className="pb-8">
             <ul className={`list-disc font-sans ml-5 ${darkMode ? 'text-white' : 'text-gray-800'}`}>
-              <li>Developed website front-end, yielding a 12% increase in page load speed and a 24% rise in user engagement.</li>
-              <li>Designed Android app UI, decreasing bounce rate by 21% and increasing user retention by 26%.</li>
-              <li>Conducted exhaustive manual testing, identifying and reporting 300+ bugs and functionality issues, successfully resolving 24%, enhancing overall user experience and system stability.</li>
+              <li>•Designed UI for the Android app and Frontend of website, resulting in a 20% increase in user satisfaction and 30% reduction in reported issues.</li>
+              <li>Conducted rigorous QA/Manual testing, uncovering and resolving 350+ major bugs and several minor fixes, enhancing application stability by 35%.</li>
+              <li>•Managed backend operations via the admin dashboard, boosting engagement by 15%, and led the 'Swaayam Seekho' initiative, driving a 25% increase in active users within six months through cross-functional team coordination.</li>
             </ul>
           </TimelineBody>
         </TimelineItem>
@@ -105,15 +107,14 @@ const WorkExp = () => {
                 DroliaFresh - Patna
               </Typography>
               <Typography className=" my-2" color={darkMode ? 'white' : 'gray'}>
-                <span style={{ fontWeight: 'bold' }}>Web Developer Intern</span> <br /> Aug'2022 - Jan'2023 (5 Months)
+                <span style={{ fontWeight: 'bold' }}>Web Developer Intern</span> <br /> May'2023 - Jul'2023
               </Typography>
             </div>
           </TimelineHeader>
           <TimelineBody className="pb-8">
             <ul className={`list-disc font-sans ml-5 ${darkMode ? 'text-white' : 'text-gray-800'}`}>
-              <li>Developed the organization's website, boosting traffic by 20% and user engagement by 10% after launch.</li>
-              <li>Generated compelling social media content, elevating follower engagement by 25% and expanding reach by 20% across platforms including Amazon, over 5 months.</li>
-              <li>Designed and executed graphic content strategies, resulting in a 30% improvement in brand recognition and a 15% uptick in conversion rates for online campaigns.</li>
+              <li>Crafted the organization's website, boosting traffic by 25% and user engagement by 10% after launch.</li>
+              <li>Produced captivating social and graphic content, boosting engagement by 40%, expanding reach by 15%, and elevating brand recognition by 25%, with a 10% increase in campaign conversions.</li>
             </ul>
           </TimelineBody>
         </TimelineItem>
